9fd73883d744ec6f61028f64a73e1f1e39fb5f4a
basantagyawali <basantagyawali01@gmail.com>
2020-12-15 18:28:43 +0545

    fixed gitignore

--- app/form.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import InputRequired
-
-class EmailForm(FlaskForm):
-	email = StringField('Email', validators=[InputRequired()])
-	submit = SubmitField('Submit') 
