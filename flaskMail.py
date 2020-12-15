8914327db8922b848b8879efaa7476bc01011d61
basantagyawali <basantagyawali01@gmail.com>
2020-12-15 17:57:29 +0545

    adding requirements file

--- flaskMail.py
+++ flaskMail.py
@@ -2,6 +2,7 @@ from app import create_app
 import os
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
+print("Inside")
 
 #if any problem occur then uncomment app.run() and run this file python 
-#app.run()
+app.run()
